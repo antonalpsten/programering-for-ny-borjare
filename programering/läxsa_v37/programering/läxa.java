d326b43c85935e4bdf44284ebe65cf7789067727
antonalpsten <anton.alpsten@elev.ga.ntig.se>
2019-09-15 23:29:44 +0200

    5

--- programering/läxsa_v37/programering/läxa.java
+++ programering/läxsa_v37/programering/läxa.java
@@ -7,6 +7,10 @@ public static void main(String[] args) {
 		Scanner scan1 = new Scanner(System.in);
 	
 		System.out.println("vad heter du?");
-		scan1.next();
+		String namn = scan1.next();
+		
+		System.out.println("hur gammal är du");
+		int gammal = scan1.nextInt();
+		
 }
 }
