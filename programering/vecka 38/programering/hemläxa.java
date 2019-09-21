8848ce30fe2435400ca1139bf7b3a3e256984be8
antonalpsten <anton.alpsten@elev.ga.ntig.se>
2019-09-21 18:28:41 +0200

    Create hemläxa.java

---
+++ programering/vecka 38/programering/hemläxa.java
@@ -0,0 +1,31 @@
+package programering;
+
+import java.util.Scanner;
+
+public class hemläxa {
+	public static void main(String[] args) {
+		Scanner input = new Scanner (System.in);
+		
+		System.out.println("skriv in ett heltal?");
+		String a = input.next();
+		System.out.println("skriv in ett till heltal?");
+		String b = input.next();
+		System.out.println("skriv in ett till heltal?");
+		String c = input.next();
+		System.out.println("skriv in ett till heltal?");
+		String d = input.next();
+		System.out.println("skriv in ett till heltal?");
+		String e = input.next();
+		System.out.println("skriv in ett till heltal?");
+		String f = input.next();
+		System.out.println("skriv in ett till heltal?");
+		String g = input.next();
+		System.out.println("skriv in ett till heltal?");
+		String h = input.next();
+		System.out.println("skriv in ett till heltal?");
+		String i = input.next();
+		System.out.println("skriv in ett till heltal?");
+		String j = input.next();
+	}
+
+}
