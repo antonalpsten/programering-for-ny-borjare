56b190a398ae53dc4095af643b34da1426e31d5c
antonalpsten <anton.alpsten@elev.ga.ntig.se>
2019-09-22 18:29:57 +0200

    Update hemläxa.java

--- programering/vecka 38/programering/hemläxa.java
+++ programering/vecka 38/programering/hemläxa.java
@@ -8,27 +8,86 @@ public class hemläxa {
 		
 		System.out.println("skriv in ett heltal?");
 		int a = input.nextInt();
+		int max = a;
+		int min = a;
 		System.out.println("skriv in ett till heltal?");
 		int b = input.nextInt();
+		if (b>max){
+			max = b;
+		}
+		if (b<min){
+			min = b;
+		}
 		System.out.println("skriv in ett till heltal?");
 		int c = input.nextInt();
+		if (c>max){
+			max = c;
+		}
+		if (c<min){
+			min = c;
+		}
 		System.out.println("skriv in ett till heltal?");
 		int d = input.nextInt();
+		if (d>max){
+			max = d;
+		}
+		if (d<min){
+			min = d;
+		}
 		System.out.println("skriv in ett till heltal?");
 		int e = input.nextInt();
+		if (e>max){
+			max = e;
+		}
+		if (e<min){
+			min = e;
+		}
 		System.out.println("skriv in ett till heltal?");
 		int f = input.nextInt();
+		if (f>max){
+			max = f;
+		}
+		if (f<min){
+			min = f;
+		}
 		System.out.println("skriv in ett till heltal?");
 		int g = input.nextInt();
+		if (g>max){
+			max = g;
+		}
+		if (g<min){
+			min = g;
+		}
 		System.out.println("skriv in ett till heltal?");
 		int h = input.nextInt();
+		if (h>max){
+			max = h;
+		}
+		if (h<min){
+			min = h;
+		}
 		System.out.println("skriv in ett till heltal?");
 		int i = input.nextInt();
+		if (i>max){
+			max = i;
+		}
+		if (i<min){
+			min = i;
+		}
 		System.out.println("skriv in ett till heltal?");
 		int j = input.nextInt();
+		if (j>max){
+			max = j;
+		}
+		if (j<min){
+			min = j;
+		}
+		
+		System.out.println("maxsnummer = " + max);
+		
+		System.out.println("minstanummer = " + min);
 		
 		
-		
-	}
 
+	}
 }
