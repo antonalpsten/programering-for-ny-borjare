59c7a0e2bcab672bb9d8e2b31f99517582cc0685
antonalpsten <anton.alpsten@elev.ga.ntig.se>
2019-09-26 10:37:23 +0200

    Quadrant

---
+++ programering/v.39/programering/Quadrant.java
@@ -0,0 +1,29 @@
+package programering;
+
+import java.util.Scanner;
+
+public class Quadrant {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("skriv ett X värde");
+		int x = input.nextInt();
+		
+		System.out.println("skriv ett Y värde");
+		int y = input.nextInt();
+		
+		if ((x>0)&&(y>0)){
+		System.out.println("Quarant 1");
+		}
+		if ((x<0)&&(y>0)){
+			System.out.println("Quarant 2");
+		}	
+		if ((x<0)&&(y<0)){
+			System.out.println("Quarant 3");
+		}	
+		if ((x>0)&&(y<0)){
+			System.out.println("Quarant 4");
+		}	
+		
+	}
+}
